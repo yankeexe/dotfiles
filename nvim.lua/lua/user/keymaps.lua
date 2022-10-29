808fdf72a521167131cbe6679138cb65486a00d9
Yankee Maharjan <yankee.exe@gmail.com>
2022-10-29 20:34:35 +0545

    Add keymap for removing highlights

--- nvim.lua/lua/user/keymaps.lua
+++ nvim.lua/lua/user/keymaps.lua
@@ -29,6 +29,8 @@ keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+keymap("n","<Space><Space>",":nohlsearch<Bar>:echo<CR>", opts)
+
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)

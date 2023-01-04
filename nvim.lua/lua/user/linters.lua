142fcfb270db4c61f4daf0d1f10b8a0637266856
Yankee <yankee.exe@gmail.com>
2023-01-04 17:06:26 +0545

    Format on save

--- nvim.lua/lua/user/linters.lua
+++ nvim.lua/lua/user/linters.lua
@@ -1,14 +1,20 @@
-local lint = require("lint")
-
-lint.linters_by_ft = {
-  python = {"mypy", "flake8"}
-}
+-- local lint = require("lint")
+--
+-- lint.linters_by_ft = {
+--   python = {"mypy", "flake8"}
+-- }
 
 -- Run linter on save
-vim.cmd [[
-au BufWritePost * lua require('lint').try_lint()
-]]
+-- vim.cmd [[
+-- au BufWritePost * lua require('lint').try_lint()
+-- ]]
 
 
 -- View error message when cursor reaches line containing the error
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+
+-- Format on Save
+vim.cmd [[
+au BufWritePost * lua vim.lsp.buf.format()
+]]

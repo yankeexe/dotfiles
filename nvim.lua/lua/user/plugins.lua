1bf0d991de3c006ebafcd09c4be6d8f1bb60f416
Yankee <yankee.exe@gmail.com>
2023-01-04 17:05:34 +0545

    Update plugin descriptions

--- nvim.lua/lua/user/plugins.lua
+++ nvim.lua/lua/user/plugins.lua
@@ -1,15 +1,15 @@
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
--- vim.cmd [[
---   augroup packer_user_config
---     autocmd!
---     autocmd BufWritePost plugins.lua source <afile> | PackerSync
---   augroup end
--- ]]
+vim.cmd [[
+   augroup packer_user_config
+     autocmd!
+     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+   augroup end
+ ]]
 
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
- --if not status_ok then
+--if not status_ok then
 -- return
 -- end
 
@@ -30,71 +30,73 @@ packer.init {
 -- cmd = lazy loading (run the extension only when you enter those commands)
 return packer.startup(function(use)
   -- cmd = lazy load plugin, only run when `:MarkdownPreview` is run.
-  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd= 'MarkdownPreview'}
+  use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview' }
 
   -- Colorschemes
   use 'folke/tokyonight.nvim'
   use "morhetz/gruvbox"
 
   -- Completion plugins
-use 'hrsh7th/cmp-buffer'
-use 'hrsh7th/cmp-path'
-use 'hrsh7th/cmp-cmdline'
-use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
 
- -- Snippet Engine
- use 'L3MON4D3/LuaSnip'
+  -- Snippet Engine
+  use 'L3MON4D3/LuaSnip'
 
--- LSP
-use {
+  -- LSP
+  use {
     'neovim/nvim-lspconfig',
     'williamboman/nvim-lsp-installer',
-}
-use 'hrsh7th/cmp-nvim-lsp'
-use 'hrsh7th/cmp-nvim-lua'
+  }
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-nvim-lua'
 
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-use {
-  "nvim-treesitter/nvim-treesitter",
-  run="<cmd>TSUpdate",
-}
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = "<cmd>TSUpdate",
+  }
 
-use "nvim-treesitter/playground"
+  use "nvim-treesitter/playground"
 
-use {
-  'romgrk/barbar.nvim',
-  requires = {'kyazdani42/nvim-web-devicons'}
-}
+  use {
+    'romgrk/barbar.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' }
+  }
 
-use {
-  'kyazdani42/nvim-tree.lua',
-  requires = {
-    'kyazdani42/nvim-web-devicons', -- optional, for file icon
-  },
-}
+  -- File Explorer side bar
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+  }
 
-use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-  require("toggleterm").setup()
-end}
+  -- Popup terminal
+  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+    require("toggleterm").setup()
+  end }
 
-
-use {
+  use {
     'numToStr/Comment.nvim',
     config = function()
-        require('Comment').setup()
+      require('Comment').setup()
     end
-}
+  }
 
-
-use 'JoosepAlviste/nvim-ts-context-commentstring'
-use "windwp/nvim-autopairs"
-use "voldikss/vim-floaterm"
-use {'mfussenegger/nvim-lint'}
-
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "windwp/nvim-autopairs"
+  use { 'simrat39/symbols-outline.nvim', config = function()
+    require("symbols-outline").setup()
+  end
+  }
 
   -- Default ones
   use "wbthomason/packer.nvim" -- Have packer manage itself
